d3ebb17e508800c4d3d066907bbb4be55e184671
Marcia Zanele Bika <211054356@mycput.ac.za>
2022-03-29 14:59:49 +0200

    added appropriate comment line

--- src/main/java/Entity/EmployeeRole.java
+++ src/main/java/Entity/EmployeeRole.java
@@ -1,3 +1,9 @@
+/* EmployeeRole.java
+   Entity for EmployeeRole
+   Author: Marcia Zanele Bika (211054356)
+   Date: 03 April 2022
+ */
+
 package Entity;
 
 public class EmployeeRole {
--- src/main/java/Entity/Role.java
+++ src/main/java/Entity/Role.java
@@ -1,3 +1,9 @@
+/* Role.java
+   Entity for Role
+   Author: Marcia Zanele Bika (211054356)
+   Date: 03 April 2022
+ */
+
 package Entity;
 
 public class Role {
