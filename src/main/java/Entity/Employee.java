d44b05ba7e28b8ed66283138410ba21947e835b6
Chadrack Mbuyi <chadrackmbuyi2@gmail.com>
2022-03-30 11:26:21 +0200

    Creation of Chadrack's new branch with the new comment line

--- src/main/java/Entity/Employee.java
+++ src/main/java/Entity/Employee.java
@@ -1,3 +1,8 @@
+/* Employee.java
+   Entity for the Employee
+   Author: Chadrack Mbuyi Kalala (219013012)
+   Date: 29 March 2022
+ */
 package Entity;
 
 public class Employee {
--- src/main/java/Entity/Restaurant.java
+++ src/main/java/Entity/Restaurant.java
@@ -1,3 +1,8 @@
+/* Restaurant.java
+   Entity for the Restaurant
+   Author: Chadrack Mbuyi Kalala (219013012)
+   Date: 29 March 2022
+ */
 package Entity;
 
 public class Restaurant {
