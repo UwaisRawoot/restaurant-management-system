0031ea462ffec1e3cff7d317657c7577a2196f28
Vuyolwethu Kalolo <Vuyolwethukalolo1994@gmail.com>
2022-03-31 01:07:19 +0200

    Fixing a class on 214015637 branch commit

---
+++ src/main/java/Entity/Reservation.java
@@ -0,0 +1,64 @@
+package Entity;
+
+public class Reservation {
+        public  int reserveId;
+        public String reserveName;
+
+     // Constructor
+
+        public Reservation(Builder builder) {
+            this.reserveId = builder.reserveId;
+            this. reserveName= builder.reserveName;
+        }
+
+        public int getReserveId() {
+            return reserveId;
+        }
+
+        public void setReserveId(int reserveId) {
+            this.reserveId = reserveId;
+        }
+
+        public String getReserveName() {
+            return reserveName;
+        }
+
+        public void setReserveName(String reserveName) {
+            this.reserveName = reserveName;
+        }
+
+        @Override
+        public String toString() {
+            return "Reservation{" +
+                    "reserveId=" + reserveId +
+                    ", reserveName='" + reserveName + '\'' +
+                    '}';
+        }
+
+        public static class Builder {
+            private int reserveId ;
+            private String reserveName;
+
+
+            public Builder setReserveId(int reserveId) {
+                this.reserveId= reserveId;
+                return this;
+            }
+
+            public Reservation.Builder setReserveName(String reserveName) {
+                this.reserveName =reserveName ;
+                return this;
+            }
+
+            public Reservation.Builder copy(Reservation reservation) {
+
+                this.reserveId = reservation.reserveId;
+                this.reserveName = reservation.reserveName;
+                return this;
+            }
+
+            public Reservation build(){
+                return new Reservation(this);
+            }
+        }
+}
