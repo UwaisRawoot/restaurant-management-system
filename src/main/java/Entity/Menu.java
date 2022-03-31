bf70fade721a750ab8ce12a8f8d05b358f38b5b6
UwaisRawoot <uwaisrawoot2@gmail.com>
2022-03-31 09:04:58 +0200

    Added descriptions

--- src/main/java/Entity/Menu.java
+++ src/main/java/Entity/Menu.java
@@ -1,3 +1,10 @@
+/**
+ * Menu.java
+ * Menu entity for Restaurant Management System
+ * Author: Uwais Ali Rawoot - 216217296
+ * Date: 31 March 2022
+ */
+
 package Entity;
 
 public class Menu {
--- src/main/java/Entity/Payment.java
+++ src/main/java/Entity/Payment.java
@@ -1,3 +1,10 @@
+/**
+ * Payment.java
+ * Payment entity for Restaurant Management System
+ * Author: Uwais Ali Rawoot - 216217296
+ * Date: 31 March 2022
+ */
+
 package Entity;
 
 public class Payment {
