1bbcd929e3693b61ba4857f947ea2ed572a873b1
UwaisRawoot <uwaisrawoot2@gmail.com>
2022-03-31 09:40:31 +0200

    Completed Menu entity

--- src/main/java/Entity/Menu.java
+++ src/main/java/Entity/Menu.java
@@ -7,5 +7,68 @@
 
 package Entity;
 
+
 public class Menu {
+
+    private int menuId;
+    private String menuItem;
+
+    //private constructor
+    private  Menu(Builder builder){
+        this.menuId = builder.menuId;
+        this.menuItem = builder.menuItem;
+    }
+
+    //Getters
+    public int getMenuId() {
+        return menuId;
+    }
+
+    public String getMenuItem() {
+        return menuItem;
+    }
+
+    //Setters
+    public void setMenuId(int menuId) {
+        this.menuId = menuId;
+    }
+
+    public void setMenuItem(String menuItem) {
+        this.menuItem = menuItem;
+    }
+
+    //toString
+    @Override
+    public String toString(){
+        return  "Menu{" +
+                ", menuId=" + menuId + '\'' +
+                ", menuItem=" + menuItem + '\'' +
+                '}';
+    }
+
+    //Builder Pattern Class
+    public static class Builder {
+        private int menuId;
+        private String menuItem;
+
+        public Builder setMenuId(int menuId){
+            this.menuId = menuId;
+            return this;
+        }
+
+        public Builder setMenuItem(String menuItem) {
+            this.menuItem = menuItem;
+            return this;
+        }
+
+        public Builder copy(Menu menu){
+            this.menuId = menuId;
+            this.menuItem = menuItem;
+            return this;
+        }
+
+        public Menu build(){
+            return new Menu(this);
+        }
+    }
 }
